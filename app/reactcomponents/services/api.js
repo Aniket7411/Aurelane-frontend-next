197a90ef33a5c5ec58eeb859eb3f971628c6fd3f
ankita-parashar15 <sharma11aniket@gmail.com>
2025-11-18 16:06:45 +0530

    updated endpoint

--- app/reactcomponents/services/api.js
+++ app/reactcomponents/services/api.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 // const API_BASE_URL = 'http://localhost:5000/api';
-const API_BASE_URL = 'https://gems-backend-u.onrender.com/api';
+const API_BASE_URL = 'https://aurelane-backend-next.onrender.com/';
 
 
 const isBrowser = typeof window !== 'undefined';
